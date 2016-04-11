357472ae6d65ed79bb734aff7f6f4695753ea0a5
Filip Juristovski <fjuristovski@gmail.com>
2016-04-10 22:28:11 -0700

    updated PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -20,21 +20,23 @@ app.get('/todos', function(req, res) {
   var query = req.query;
   var where = {};
 
-  if (query.hasOwnProperty('completed') && query.completed === 'true'){
+  if (query.hasOwnProperty('completed') && query.completed === 'true') {
     where.completed = true;
-  } else if (query.hasOwnProperty('completed') && query.completed === 'false'){
+  } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
     where.completed = false;
   }
 
-  if (query.hasOwnProperty('q') && query.q.length > 0){
+  if (query.hasOwnProperty('q') && query.q.length > 0) {
     where.description = {
       $like: '%' + query.q + '%'
     }
   }
 
-  db.todo.findAll({where: where}).then(function(todos){
-      res.json(todos); 
-  }, function(e){
+  db.todo.findAll({
+    where: where
+  }).then(function(todos) {
+    res.json(todos);
+  }, function(e) {
     res.status(500).send();
   });
 
@@ -63,13 +65,13 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
   var todoId = req.params.id
 
-  db.todo.findById(todoId).then(function(todo){
-    if (!!todo){ //!! converts an object to truthy or falsey
+  db.todo.findById(todoId).then(function(todo) {
+    if (!!todo) { //!! converts an object to truthy or falsey
       res.json(todo.toJSON());
     } else {
       res.status(404).send();
     }
-  }, function(e){
+  }, function(e) {
     res.status(500).send();
   });
 
@@ -92,9 +94,9 @@ app.post('/todos', function(req, res) {
   var body = _.pick(req.body, 'description', 'completed'); //req.body;
   // console.log("description: " + body.description);
 
-  db.todo.create(body).then(function(todo){
+  db.todo.create(body).then(function(todo) {
     res.json(todo.toJSON());
-  }, function(e){
+  }, function(e) {
     res.status(400).json(e);
   });
 
@@ -134,15 +136,15 @@ app.delete('/todos/:id', function(req, res) { //use _.without
     where: {
       id: toDoId
     }
-  }).then(function(rowsDeleted){
-    if (rowsDeleted === 0 ){
+  }).then(function(rowsDeleted) {
+    if (rowsDeleted === 0) {
       res.status(404).json({
         error: 'No todo with id'
       });
     } else {
       res.status(204).send();
     }
-  }, function(){
+  }, function() {
     res.status(500).send();
   });
 
@@ -165,32 +167,34 @@ app.delete('/todos/:id', function(req, res) { //use _.without
 // Put /todos/:id
 app.put('/todos/:id', function(req, res) {
   var body = _.pick(req.body, 'description', 'completed'); //req.body;
-  var validAttributes = {};
-
+  var attributes = {};
   var toDoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {
-    id: toDoId
-  }); //todos[toDoId];
 
-  if (!matchedTodo) {
-    return res.status(404).send();
+  if (body.hasOwnProperty('completed')) {
+    attributes.completed = body.completed;
   }
 
-  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if (body.hasOwnProperty('completed')) {
-    return res.status(400).send();
+  if (body.hasOwnProperty('description')) {
+    attributes.description = body.description;
   }
 
-  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-    validAttributes.description = body.description;
-  } else if (body.hasOwnProperty('description')) {
-    return res.status(404).send();
-  }
+  db.todo.findById(toDoId).then(function(todo) {
+    if (todo) {
+      todo.update(attributes).then(function(todo) {
+        res.json(todo.toJSON());
+      }, function(e) {
+        res.status(400).json(e);
+      });
+    } else {
+      res.status(404).send();
+    }
+  }, function() {
+    res.status(500).send();
+  });
 
-  _.extend(matchedTodo, validAttributes);
-  //objects in JS are passed by reference
-  res.json(matchedTodo);
+  // _.extend(matchedTodo, attributes);
+  // //objects in JS are passed by reference
+  // res.json(matchedTodo);
 
 });
 
